af1363c9e981a282c09e2c99a2a1a751c404e3d3
rs22095L.nago4 <120536850+nago4@users.noreply.github.com>
2024-10-23 10:20:59 +0900

    カテゴリごとのリンクの取得

--- scrape_and_save.py
+++ scrape_and_save.py
@@ -3,19 +3,19 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()  # ブラウザの起動
-driver.get('https://cookpad.com/jp')
+driver.get('https://cookpad.com/jp/categories')
 
-# rel="nofollow"のaタグのhref属性を取得
-elements = driver.find_elements(By.XPATH, "//a[@href ]")
+# 特定のクラスを持つ<li>要素内の<a>タグのhref属性を取得
+elements = driver.find_elements(By.XPATH, "//li[contains(@class, 'block-link flex gap-sm py-sm items-center border-b border-cookpad-gray-400')]//a[@class='block-link__main text-cookpad-16 font-semibold']")
 links = [element.get_attribute("href") for element in elements]
 
 # リンクをテキストファイルに保存
-with open('nofollow_links.txt', 'w', encoding='utf-8') as file:
+with open('links.txt', 'w', encoding='utf-8') as file:
     for link in links:
         file.write(link + '\n')
 
 # ブラウザを開いたままにするために待機
-time.sleep(5)  # 30秒間待機
+time.sleep(5)  # 5秒間待機
 
 # ブラウザを閉じる
 driver.quit()
